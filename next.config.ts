4e1fb3a249e3e6a77065c1eb8fa5dd26738f9b43
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-05-15 12:19:55 +0200

    Legg til en hack for å bygge oasis.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     output: 'standalone',
     basePath: '/barnetrygd/min-barnetrygd',
+    // En hack for å bygge oasis. Burde løses med riktig config i prosjektet
+    webpack: (config) => {
+        config.externals.push({
+            'node:crypto': 'commonjs crypto',
+        });
+        return config;
+    },
 };
 
 export default nextConfig;
