a53feaf74d34656fda2a9131b9dd2eb4ecefb98b
thoalm <thoalm@outlook.com>
2025-06-20 09:39:32 +0200

    NAV-24639: Refaktorer seksjon til å ta i bruk et compound component design pattern.

--- src/app/komponenter/Seksjon.tsx
+++ src/app/komponenter/Seksjon.tsx
@@ -1,36 +1,48 @@
-import { Heading, HGrid, HGridProps, VStack } from '@navikt/ds-react';
+import { BoxProps, Heading, HGrid, HGridProps, VStack } from '@navikt/ds-react';
 import { PageBlock } from '@navikt/ds-react/Page';
 import { Box } from '@navikt/ds-react';
-import React, { ReactNode } from 'react';
+import React, { PropsWithChildren } from 'react';
 
-interface Props {
-    tittel?: ReactNode;
-    children?: ReactNode;
-    antallKolonner?: HGridProps['columns'];
-    gråBakgrunn?: boolean;
+interface SeksjonProps {
+    bakgrunn?: BoxProps['background'];
+    children: React.ReactNode;
 }
 
-const Seksjon: React.FC<Props> = ({ tittel, children, antallKolonner, gråBakgrunn }) => {
-    const content = (
-        <PageBlock as="section" width="lg" gutters>
-            <VStack
-                marginBlock={{ xs: '14 0', lg: '16 0' }}
-                paddingBlock={gråBakgrunn ? { xs: '14', lg: '16' } : '0'}
-                gap={{ xs: '4', lg: '6' }}
-            >
-                {tittel && (
-                    <Heading level="2" size="medium">
-                        {tittel}
-                    </Heading>
-                )}
-                <HGrid columns={antallKolonner || 1} gap={{ xs: '4', lg: '6' }}>
+export function Seksjon({ bakgrunn, children }: SeksjonProps) {
+    return (
+        <Box background={bakgrunn}>
+            <PageBlock as={'section'} width={'lg'} gutters={true}>
+                <VStack
+                    marginBlock={{ xs: '14 0', lg: '16 0' }}
+                    paddingBlock={{ xs: '14', lg: '16' }}
+                    gap={{ xs: '4', lg: '6' }}
+                >
                     {children}
-                </HGrid>
-            </VStack>
-        </PageBlock>
+                </VStack>
+            </PageBlock>
+        </Box>
     );
+}
 
-    return gråBakgrunn ? <Box background="surface-subtle">{content}</Box> : content;
-};
+interface InnholdProps extends PropsWithChildren {
+    kolonner?: HGridProps['columns'];
+}
 
-export default Seksjon;
+function SeksjonInnhold({ kolonner = 1, children }: InnholdProps) {
+    return (
+        <HGrid columns={kolonner} gap={{ xs: '4', lg: '6' }}>
+            {children}
+        </HGrid>
+    );
+}
+
+function SeksjonTittel({ children }: PropsWithChildren) {
+    return (
+        <Heading level={'2'} size={'medium'}>
+            {children}
+        </Heading>
+    );
+}
+
+Seksjon.Tittel = SeksjonTittel;
+Seksjon.Innhold = SeksjonInnhold;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import {
 } from '@navikt/aksel-icons';
 import { BodyLong, BodyShort, Heading, HStack, Link } from '@navikt/ds-react';
 
-import Seksjon from './komponenter/Seksjon';
+import { Seksjon } from './komponenter/Seksjon';
 import LenkePanel from './komponenter/LenkePanel';
 import Dokumentoversikt from './komponenter/Dokumentoversikt';
 import YtelseKort from './komponenter/YtelseKort';
@@ -21,100 +21,114 @@ export default function Page() {
     return (
         <>
             <Seksjon>
-                <HStack gap="2">
-                    <HouseIcon title="Min side" fontSize="1.5rem" />
-                    <BodyShort>Min side</BodyShort>
-                    <ChevronRightIcon title="Neste" fontSize="1.5rem" />
-                    <BodyShort>Barnetrygden min</BodyShort>
-                </HStack>
-                <Heading level="1" size="large">
-                    <HStack align="center" gap="6">
-                        <BarnetrygdMedium />
-                        <span>Barnetrygden min</span>
+                <Seksjon.Innhold>
+                    <HStack gap="2">
+                        <HouseIcon title="Min side" fontSize="1.5rem" />
+                        <BodyShort>Min side</BodyShort>
+                        <ChevronRightIcon title="Neste" fontSize="1.5rem" />
+                        <BodyShort>Barnetrygden min</BodyShort>
                     </HStack>
-                </Heading>
+                    <Heading level="1" size="large">
+                        <HStack align="center" gap="6">
+                            <BarnetrygdMedium />
+                            <span>Barnetrygden min</span>
+                        </HStack>
+                    </Heading>
+                </Seksjon.Innhold>
             </Seksjon>
 
-            <Seksjon
-                tittel="Ønsker du å kontakte oss?"
-                antallKolonner={{ md: 1, lg: 2 }}
-                gråBakgrunn
-            >
-                <LenkePanel
-                    href="#"
-                    tittel="Skriv til oss"
-                    ikon={<Chat2Icon title="Snakkeboble" fontSize="2rem" />}
-                >
-                    <BodyLong textColor="subtle">
-                        Har du spørsmål til saken din eller vil melde fra om noe annet?
-                    </BodyLong>
-                </LenkePanel>
-
-                <LenkePanel
-                    href="#"
-                    tittel="Meld fra om endringer"
-                    ikon={<GavelIcon title="Endringer i din situasjon" fontSize="2rem" />}
-                >
-                    <BodyLong textColor="subtle">
-                        Endring i din situasjon, opphold eller arbeidsforhold i utlandet.
-                    </BodyLong>
-                </LenkePanel>
+            <Seksjon bakgrunn={'surface-subtle'}>
+                <Seksjon.Tittel>Ønsker du å kontakte oss?</Seksjon.Tittel>
+                <Seksjon.Innhold kolonner={{ md: 1, lg: 2 }}>
+                    <LenkePanel
+                        href="#"
+                        tittel="Skriv til oss"
+                        ikon={<Chat2Icon title="Snakkeboble" fontSize="2rem" />}
+                    >
+                        <BodyLong textColor="subtle">
+                            Har du spørsmål til saken din eller vil melde fra om noe annet?
+                        </BodyLong>
+                    </LenkePanel>
+                    <LenkePanel
+                        href="#"
+                        tittel="Meld fra om endringer"
+                        ikon={<GavelIcon title="Endringer i din situasjon" fontSize="2rem" />}
+                    >
+                        <BodyLong textColor="subtle">
+                            Endring i din situasjon, opphold eller arbeidsforhold i utlandet.
+                        </BodyLong>
+                    </LenkePanel>
+                </Seksjon.Innhold>
             </Seksjon>
 
-            <Seksjon tittel="Snarveier" antallKolonner={{ md: 1, lg: 2 }}>
-                <LenkePanel
-                    href="#"
-                    tittel="Slik klager du"
-                    ikon={<ThumbDownIcon title="Klage" fontSize="2rem" />}
-                    graBakgrunn
-                >
-                    <BodyLong textColor="subtle">
-                        Du har rett til å klage eller anke hvis du er uenig i vedtaket.
-                    </BodyLong>
-                </LenkePanel>
+            <Seksjon>
+                <Seksjon.Tittel>Snarveier</Seksjon.Tittel>
+                <Seksjon.Innhold kolonner={{ md: 1, lg: 2 }}>
+                    <LenkePanel
+                        href="#"
+                        tittel="Slik klager du"
+                        ikon={<ThumbDownIcon title="Klage" fontSize="2rem" />}
+                        graBakgrunn
+                    >
+                        <BodyLong textColor="subtle">
+                            Du har rett til å klage eller anke hvis du er uenig i vedtaket.
+                        </BodyLong>
+                    </LenkePanel>
 
-                <LenkePanel
-                    href="#"
-                    tittel="Saksbehandlingstider"
-                    ikon={<ClockIcon title="Saksbehandlingstid" fontSize="2rem" />}
-                    graBakgrunn
-                >
-                    <BodyLong textColor="subtle">
-                        Se saksbehandlingstider og ventetider for barnetrygd.
-                    </BodyLong>
-                </LenkePanel>
+                    <LenkePanel
+                        href="#"
+                        tittel="Saksbehandlingstider"
+                        ikon={<ClockIcon title="Saksbehandlingstid" fontSize="2rem" />}
+                        graBakgrunn
+                    >
+                        <BodyLong textColor="subtle">
+                            Se saksbehandlingstider og ventetider for barnetrygd.
+                        </BodyLong>
+                    </LenkePanel>
+                </Seksjon.Innhold>
             </Seksjon>
 
-            <Seksjon tittel="Dokumentoversikt" gråBakgrunn>
-                <BodyLong>
-                    Her finner du alle søknader, vedlegg, vedtak, brev, samtalereferater og
-                    meldinger.
-                </BodyLong>
-                <Dokumentoversikt />
-                <Link href="#">Har du sendt en søknad eller et dokument som ikke vises her?</Link>
+            <Seksjon bakgrunn={'surface-subtle'}>
+                <Seksjon.Tittel>Dokumentoversikt</Seksjon.Tittel>
+                <Seksjon.Innhold>
+                    <BodyLong>
+                        Her finner du alle søknader, vedlegg, vedtak, brev, samtalereferater og
+                        meldinger.
+                    </BodyLong>
+                    <Dokumentoversikt />
+                    <Link href="#">
+                        Har du sendt en søknad eller et dokument som ikke vises her?
+                    </Link>
+                </Seksjon.Innhold>
             </Seksjon>
 
-            <Seksjon tittel="Dette kan du ha rett til" antallKolonner={{ sm: 1, md: 2, lg: 3 }}>
-                <YtelseKort
-                    tittel="For deg som er helt eller delvis alene med barn"
-                    ikon={<AleneSmall />}
-                >
-                    Når du er alene med barn, finnes det ulike støtteordninger du kan ha rett til.
-                    Hvilke ordninger du har rett til, avhenger blant annet av barnets alder,
-                    sivilstanden din og bo- og arbeidssituasjonen din.
-                </YtelseKort>
+            <Seksjon>
+                <Seksjon.Tittel>Dette kan du ha rett til</Seksjon.Tittel>
+                <Seksjon.Innhold kolonner={{ sm: 1, md: 2, lg: 3 }}>
+                    <YtelseKort
+                        tittel="For deg som er helt eller delvis alene med barn"
+                        ikon={<AleneSmall />}
+                    >
+                        Når du er alene med barn, finnes det ulike støtteordninger du kan ha rett
+                        til. Hvilke ordninger du har rett til, avhenger blant annet av barnets
+                        alder, sivilstanden din og bo- og arbeidssituasjonen din.
+                    </YtelseKort>
 
-                <YtelseKort
-                    tittel="Les mer om utvidet barnetrygd"
-                    ikon={<UtvidetBarnetrygdSmall />}
-                >
-                    Et tillegg til ordinær barnetrygd når du bor alene med barn under 18 år.
-                </YtelseKort>
+                    <YtelseKort
+                        tittel="Les mer om utvidet barnetrygd"
+                        ikon={<UtvidetBarnetrygdSmall />}
+                    >
+                        Et tillegg til ordinær barnetrygd når du bor alene med barn under 18 år.
+                    </YtelseKort>
 
-                <YtelseKort tittel="Har ansvar for andres barn" ikon={<AnsvarForAndresBarnSmall />}>
-                    Om pengestøtter for deg som tar vare på andres barn som fosterforelder eller
-                    annen omsorgsperson.
-                </YtelseKort>
+                    <YtelseKort
+                        tittel="Har ansvar for andres barn"
+                        ikon={<AnsvarForAndresBarnSmall />}
+                    >
+                        Om pengestøtter for deg som tar vare på andres barn som fosterforelder eller
+                        annen omsorgsperson.
+                    </YtelseKort>
+                </Seksjon.Innhold>
             </Seksjon>
         </>
     );
