2dbfdcff192eadbd536a9ea36724cfcdeaba3162
Hanne Olsen <hanne.olsen@nav.no>
2025-07-07 16:07:43 +0200

    Kjører opp MSW for kall som går via NextJS når vi er i localhost

--- src/test/mock/handlers.ts
+++ src/test/mock/handlers.ts
@@ -1,3 +1,5 @@
+import { HentMinSideBarnetrygd } from '@/app/typer/api/Barnetrygd';
+import { hentFamilieBaSakBaseUrl } from '@/app/util/miljø';
 import { http, HttpResponse } from 'msw';
 import { HentMinSideBarnetrygdSuksessDto } from '@/app/typer/api/Barnetrygd';
 
@@ -10,5 +12,17 @@ export const handlers = [
                 },
             },
         });
+    }),
+    http.get(hentFamilieBaSakBaseUrl() + '/api/minside/barnetrygd', () => {
+        return HttpResponse.json<HentMinSideBarnetrygd>(
+            {
+                barnetrygd: {
+                    ordinær: {
+                        startmåned: '2024-10',
+                    },
+                },
+            },
+            { status: 200 }
+        );
     }),
 ];
