ba6685a0a96f6624c185a37211bad6372dc35ac7
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2025-07-28 14:58:00 +0200

    Legger til a11y tester

---
+++ src/app/page.test.tsx
@@ -0,0 +1,23 @@
+import { describe, it, expect } from 'vitest';
+import { render } from '@/test/testrender';
+import { axe, toHaveNoViolations } from 'jest-axe';
+import Page from './page';
+
+expect.extend(toHaveNoViolations);
+
+describe('Page', () => {
+    it('viser overskrifter', () => {
+        const { screen } = render(<Page />);
+        expect(screen.getByRole('heading', { name: /Barnetrygden min/i })).toBeInTheDocument();
+        expect(screen.getByText('Ønsker du å kontakte oss?')).toBeInTheDocument();
+        expect(screen.getByText('Snarveier')).toBeInTheDocument();
+        expect(screen.getByText('Dokumentoversikt')).toBeInTheDocument();
+        expect(screen.getByText('Dette kan du ha rett til')).toBeInTheDocument();
+    });
+
+    it('har ingen a11y-feil', async () => {
+        const { container } = render(<Page />);
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
+    });
+});
