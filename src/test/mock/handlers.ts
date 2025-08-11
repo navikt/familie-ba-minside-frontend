cc6e9da4229ef6cc0c85c940a8f10f80373095eb
Hanne Olsen <hanne.olsen@nav.no>
2025-08-11 13:55:16 +0200

    Legger til mocking av henting av dokumenter med MSW

--- src/test/mock/handlers.ts
+++ src/test/mock/handlers.ts
@@ -1,6 +1,7 @@
 import { HentMinSideBarnetrygdSuksess } from '@/typer/api/barnetrygd';
 import { hentFamilieBaSakBaseUrl } from '@/util/miljø';
 import { http, HttpResponse } from 'msw';
+import { dokumenterFraSaf } from './mockdata/dokumenter';
 
 export const handlers = [
     http.get('http://fakedings.intern.dev.nav.no/fake/tokenx', () => {
@@ -18,4 +19,10 @@ export const handlers = [
             { status: 200 }
         );
     }),
+    http.get(
+        'https://familie-integrasjoner.dev-fss-pub.nais.io/api/journalpostselvbetjening/dokumentoversikt/BAR',
+        () => {
+            return HttpResponse.json(dokumenterFraSaf, { status: 200 });
+        }
+    ),
 ];
