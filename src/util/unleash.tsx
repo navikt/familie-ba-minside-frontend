0ca6be83c9488fbf6bf423aeed886b8fad998d6e
Hanne Olsen <hanne.olsen@nav.no>
2025-08-12 15:03:15 +0200

    Legger til unleash og kill switch

---
+++ src/util/unleash.tsx
@@ -0,0 +1,28 @@
+import { startUnleash, Unleash } from 'unleash-client';
+
+export enum UnleashToggle {
+    'DISABLE_MINSIDE' = 'familie-ba-minside-frontend.disable-minside',
+}
+
+const url = process.env.UNLEASH_SERVER_API_URL
+    ? process.env.UNLEASH_SERVER_API_URL + '/api'
+    : 'https://teamfamilie-unleash-api.nav.cloud.nais.io/api';
+
+let unleash: Unleash;
+
+export async function initialiserUnleash() {
+    if (!unleash) {
+        unleash = await startUnleash({
+            url: url,
+            appName: 'familie-ba-minside-frontend',
+            customHeaders: { Authorization: process.env.UNLEASH_SERVER_API_TOKEN ?? '' },
+        });
+    }
+}
+
+export function erToggleSkruddPå(toggle: UnleashToggle) {
+    if (!unleash) {
+        throw Error('Unleash må være initialisert før vi kan bruke toggles');
+    }
+    return unleash.isEnabled(toggle);
+}
