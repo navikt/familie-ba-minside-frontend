77ed39f20eaee957ba3116c67e777c1b8ee6ccff
Hanne Olsen <hanne.olsen@nav.no>
2025-08-19 09:00:08 +0200

    Ny tekst på killswitch-side (#110)

--- src/komponenter/KillSwitch.tsx
+++ src/komponenter/KillSwitch.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from 'react';
 import { erToggleSkruddPå, UnleashToggle } from '@/util/unleash';
-import { BodyShort, Button, Heading, Link, VStack } from '@navikt/ds-react';
+import { BodyShort, Button, Heading, VStack } from '@navikt/ds-react';
 import { Seksjon } from '@/komponenter/Seksjon';
 
 export function KillSwitch(props: PropsWithChildren) {
@@ -13,26 +13,13 @@ export function KillSwitch(props: PropsWithChildren) {
                     <VStack gap="16">
                         <VStack gap="12" align="start">
                             <div>
-                                <BodyShort textColor="subtle" size="small">
-                                    Statuskode 500
-                                </BodyShort>
                                 <Heading level="1" size="large" spacing>
-                                    Beklager, noe gikk galt.
+                                    Vi jobber med å forbedre siden
                                 </Heading>
-                                <BodyShort spacing>
-                                    En teknisk feil på våre servere gjør at siden er utilgjengelig.
-                                    Dette skyldes ikke noe du gjorde.
-                                </BodyShort>
-                                <BodyShort spacing>
-                                    Du kan prøve å vente noen minutter og{' '}
-                                    <Link href="">laste siden på nytt</Link>
-                                </BodyShort>
                                 <BodyShort>
-                                    Hvis problemet vedvarer, kan du{' '}
-                                    <Link href="https://nav.no/kontaktoss" target="_blank">
-                                        kontakte oss (åpnes i ny fane)
-                                    </Link>
-                                    .
+                                    Denne siden er midlertidig utilgjengelig mens vi legger til nytt
+                                    innhold og gjør forbedringer. Takk for tålmodigheten – prøv
+                                    gjerne igjen litt senere.
                                 </BodyShort>
                             </div>
                             <Button as="a" href="https://www.nav.no/minside">
@@ -41,17 +28,12 @@ export function KillSwitch(props: PropsWithChildren) {
                         </VStack>
                         <div>
                             <Heading level="1" size="large" spacing>
-                                Something went wrong
+                                We’re working on this page
                             </Heading>
-                            <BodyShort spacing>
-                                This was caused by a technical fault on our servers. Please refresh
-                                this page or try again in a few minutes.{' '}
-                            </BodyShort>
                             <BodyShort>
-                                <Link target="_blank" href="https://www.nav.no/kontaktoss/en">
-                                    Contact us (opens in new tab)
-                                </Link>{' '}
-                                if the problem persists.
+                                This page is temporarily unavailable while we add new content and
+                                improvements. Thank you for your patience – please check back a bit
+                                later.
                             </BodyShort>
                         </div>
                     </VStack>
