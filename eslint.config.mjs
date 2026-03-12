4d7491901cc899987d9b417c619ad25391b53399
thoalm <thoalm@outlook.com>
2026-03-12 15:53:53 +0100

    Tilpasser kode for next v16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,17 @@
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-});
 
 const eslintConfig = [
     eslintConfigPrettier,
-    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    ...nextCoreWebVitals,
+    ...nextTypescript,
+    {
+        ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+        rules: {
+            'react-hooks/set-state-in-effect': 'warn', // TODO : Sett til error når problemene er fikset
+        },
+    },
 ];
 
 export default eslintConfig;
